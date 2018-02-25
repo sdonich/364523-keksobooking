ee411a68dd94eb4c0f440c6591dd989a3bfd2083
sdonich <sdonich@gmail.com>
2018-02-25 15:37:49 +0200

    Личный проект: перламутровые пуговицы (v 1.0)

--- js/pin.js
+++ js/pin.js
@@ -1,10 +1,30 @@
 'use strict';
 
 (function (global) {
+  var MAX_MAP_PINS = 5;
   var mapPins = document.querySelector('.map__pins');
-  var adverts = [];
+  var loadAdverts = [];
   var fragmentMapPin = document.createDocumentFragment();
   var similaradvertsTemplate = document.querySelector('template').content.querySelector('.map__pin');
+
+  var cleanMapPins = function () {
+    while (mapPins.children[2]) {
+      mapPins.removeChild(mapPins.children[2]);
+    }
+  };
+
+  var formContainer = document.querySelector('.map__filters');
+  formContainer.addEventListener('change', function () {
+    cleanMapPins();
+    removePopup();
+    window.debounce(global.createMapPins);
+  });
+
+  var removePopup = function () {
+    if (document.querySelector('.popup')) {
+      document.querySelector('.popup').remove();
+    }
+  };
 
   var renderMapPin = function (pin) {
     var mapPin = similaradvertsTemplate.cloneNode(true);
@@ -15,17 +35,21 @@
     return mapPin;
   };
 
+  var chooseMapPin = function () {
+    var adverts = window.useFilter(loadAdverts);
+
+    for (var i = 0; i < MAX_MAP_PINS && i < adverts.length; i++) {
+      var pin = renderMapPin(adverts[i]);
+      window.setupPinHandler(pin, adverts[i]);
+      fragmentMapPin.appendChild(pin);
+    }
+    return fragmentMapPin;
+  };
+
   global.createMapPins = function () {
     window.backend.load(function (response) {
-      adverts = response.slice();
-
-      for (var i = 0; i < adverts.length; i++) {
-        var pin = renderMapPin(adverts[i]);
-
-        window.setupPinHandler(pin, adverts[i]);
-        fragmentMapPin.appendChild(pin);
-      }
-      mapPins.appendChild(fragmentMapPin);
+      loadAdverts = response.slice();
+      mapPins.appendChild(chooseMapPin());
     },
     window.notice.error);
   };
