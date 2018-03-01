cb46ae28cf9816078ca36ac2294fd2c0615e1ce8
sdonich <sdonich@gmail.com>
2018-03-01 10:21:08 +0200

    Финальная доработка (v 0.9.2)

--- js/coordinate.js
+++ js/coordinate.js
@@ -8,23 +8,23 @@
   var START_Y = 375;
   var MAIN_X = START_X + MAIN_PIN_RADIUS;
   var MAIN_Y = START_Y + MAIN_PIN_RADIUS;
-  
+
   var mainPin = document.querySelector('.map__pin--main');
   var formNotice = document.querySelector('.notice__form');
   var fieldsets = formNotice.querySelectorAll('fieldset');
   var mainPinAdress = fieldsets[2].querySelector('input');
-  
+
   global.getCoords = {
-    start: function() {
+    start: function () {
       mainPinAdress.value = MAIN_X + ', ' + MAIN_Y;
       mainPin.style.left = START_X + 'px';
       mainPin.style.top = START_Y + 'px';
     },
-    form: function() {
-    var mainCoordinateX = mainPin.offsetLeft + MAIN_PIN_RADIUS;
-    var mainCoordinateY = mainPin.offsetTop + MAIN_PIN_RADIUS + NEEDLE;
-    mainPinAdress.value = mainCoordinateX + ', ' + mainCoordinateY;
+    form: function () {
+      var mainCoordinateX = mainPin.offsetLeft + MAIN_PIN_RADIUS;
+      var mainCoordinateY = mainPin.offsetTop + MAIN_PIN_RADIUS + NEEDLE;
+      mainPinAdress.value = mainCoordinateX + ', ' + mainCoordinateY;
     }
-  }
+  };
 
 })(window);
